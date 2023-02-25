f1f3b2c541b757ff0b5f3f1563041f2732694058
alexm <alex-m@outlook.com>
2023-02-25 13:29:19 -0800

    Put image link into variable; add main block; add code to connect to database

--- seed.py
+++ seed.py
@@ -6,25 +6,30 @@
 Seed file for Cupcakes app - adds initial data to database.
 """
 
-from app import app
+from app import app, connect_db
 from models import db, Cupcake
 
+IMG1 = ("https://www.bakedbyrachel.com/wp-content/uploads/2018/01/"
+        + "chocolatecupcakesccfrosting1_bakedbyrachel.jpg")
 
-db.drop_all()
-db.create_all()
 
-c1 = Cupcake(
-    flavor="cherry",
-    size="large",
-    rating=5,
-)
+if __name__ == "__main__":
 
-c2 = Cupcake(
-    flavor="chocolate",
-    size="small",
-    rating=9,
-    image="https://www.bakedbyrachel.com/wp-content/uploads/2018/01/chocolatecupcakesccfrosting1_bakedbyrachel.jpg"
-)
+    connect_db(app)
 
-db.session.add_all([c1, c2])
-db.session.commit()
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+
+        # Create cupcakes
+        c1 = Cupcake(flavor="cherry",
+                     size="large",
+                     rating=5)
+
+        c2 = Cupcake(flavor="chocolate",
+                     size="small",
+                     rating=9,
+                     image=IMG1)
+
+        db.session.add_all([c1, c2])
+        db.session.commit()
