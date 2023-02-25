3259aa4b806d2caaea8a0c672f2f5b321b83e1ab
alexm <alex-m@outlook.com>
2023-02-25 13:40:01 -0800

    Update module docstring; set up Flask app, debug toolbar, and connect app to database

--- app.py
+++ app.py
@@ -3,5 +3,20 @@
 # Cupcakes
 
 """
-Flask app for Cupcakes.
+Flask app for Cupcakes: route and view definitions.
 """
+
+from flask import Flask, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db, Cupcake
+
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "O secreta secreta, of doamne"
+debug = DebugToolbarExtension(app)
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///cupcakes"
+# app.config['SQLALCHEMY_ECHO'] = True
