20b6ab59d1b3ddbcc981fa06068d069bb379304d
alexm <alex-m@outlook.com>
2023-02-25 13:53:26 -0800

    Create Cupcake model and define its columns

--- models.py
+++ models.py
@@ -10,6 +10,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+DEFAULT_IMG = "https://tinyurl.com/demo-cupcake"
+
 
 def connect_db(app):
     """
@@ -18,3 +20,17 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+
+class Cupcake(db.Model):
+    """
+    Model for a cupcake.
+    """
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.String(30), nullable=False)
+    size = db.Column(db.String(30), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False, server_default=DEFAULT_IMG)
