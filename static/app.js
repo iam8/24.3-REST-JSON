55e1d1695214f1baeb630886f7f753611b43344d
alexm <alex-m@outlook.com>
2023-02-27 16:49:42 -0800

    Add function stubs and docstrings for getting all cupcakes and for handle adding new cupcake; add 'main' block (DOM loaded)

--- static/app.js
+++ static/app.js
@@ -6,3 +6,23 @@
  * Front-end for cupcakes app: query the cupcakes API and handle updating of the homepage when a
  * new cupcake is added.
  */
+
+/**
+ * Retrieve list of cupcakes from API and display on page.
+ */
+function get_all_cupcakes() {
+
+}
+
+
+/**
+ * Handle new cupcake form submission and update homepage to display new cupcake data.
+ */
+function handle_add_new_cupcake() {
+
+}
+
+
+$(function() {
+    console.log("DOM loaded");
+})
