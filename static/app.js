1796bbd814078e29034dd1a7728e1450d5680bb0
alexm <alex-m@outlook.com>
2023-02-27 17:16:22 -0800

    Create necessary jQuery elements; rename functions; add preliminary code (and some testing code) for all functions; make all function async

--- static/app.js
+++ static/app.js
@@ -7,22 +7,46 @@
  * new cupcake is added.
  */
 
+// jQuery elements
+$cupcakeList = $("#all-cupcakes")
+$newCupcakeForm = $("#add-form");
+$flavorInput = $("#flavor");
+$sizeInput = $("#size");
+$ratingInput = $("#rating");
+$imageInput = $("#image");
+
+
 /**
  * Retrieve list of cupcakes from API and display on page.
  */
-function get_all_cupcakes() {
-
+async function updateCupcakeList() {
+    $cupcakeList.empty();
 }
 
 
 /**
  * Handle new cupcake form submission and update homepage to display new cupcake data.
  */
-function handle_add_new_cupcake() {
+async function handle_add_new_cupcake(event) {
+    event.preventDefault();
 
+    console.log("Form has been submitted");
+
+    // TODO: make AJAX request to cupcakes API to add new cupcake using form data
+    const flavor = $flavorInput.val();
+    const size = $sizeInput.val();
+    const rating = $ratingInput.val();
+    const image = $imageInput.val();
+
+    await updateCupcakeList();
+
+    console.log(flavor, size, rating, image);
 }
 
 
-$(function() {
+$(async function() {
     console.log("DOM loaded");
+
+    await updateCupcakeList();
+    $newCupcakeForm.on("submit", handle_add_new_cupcake);
 })
