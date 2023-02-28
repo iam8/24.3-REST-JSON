e65f0aba64fa2386911078e4b59c1f132d4357c0
alexm <alex-m@outlook.com>
2023-02-27 16:41:15 -0800

    Add JS file for app

---
+++ static/app.js
@@ -0,0 +1,8 @@
+// Ioana A Mititean
+// Exercise 24.3: REST and JSON
+// Cupcakes
+
+/**
+ * Front-end for cupcakes app: query the cupcakes API and handle updating of the homepage when a
+ * new cupcake is added.
+ */
